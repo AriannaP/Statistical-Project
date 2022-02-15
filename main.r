6aae5ac564d4e6383ef5e13b54de593699e5d900
AriannaP <99764714+AriannaP@users.noreply.github.com>
2022-02-15 21:51:10 +0100

    Create main.r

---
+++ main.r
@@ -0,0 +1,15 @@
+#video youtube R Programming Tutorial 
+
+#unload Data
+
+library(datasets) #import library
+
+###################################
+
+#summarize Data
+
+head(iris) #shows me first six lines of datasets
+ 
+summary(iris) #summary statistic for iris data
+
+plot(iris) #scatterplot matric for iris data
